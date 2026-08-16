4d2ac19bdef2bc1065f2f09aff640fa67e6a9e7e
radwaadel17 <radwaadel959@gmail.com>
2026-08-16 18:37:03 +0300

    feat: Add ColorsApp utility class for color constants

--- lib/HomePage/presentation/widgets/music-streaming-view-body.dart
+++ lib/HomePage/presentation/widgets/music-streaming-view-body.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:streamingapp/utils/widgets/adaptive-layouts.dart';
 import 'package:streamingapp/utils/widgets/desktop-layout.dart';
+import 'package:streamingapp/utils/widgets/mobile-layout.dart';
 import 'package:streamingapp/utils/widgets/tablet-layout.dart';
-import 'package:streamingapp/widgets/adaptive-layouts.dart';
-import 'package:streamingapp/widgets/mobile-layout.dart';
+
 
 class MusicStreamingViewBody extends StatelessWidget {
   const MusicStreamingViewBody({super.key});
---
+++ lib/utils/colors-app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp{
+  static const Color secondaryColor = Color(0xFF272727);
+  static const Color blueColor = Color(0xFF2E3A59);
+  static const Color whiteColor = Colors.white;
+  static const Color redColor = Color(0xFFF3777D);
+  static const Color grey = Color(0xFF959D99);
+  static const Color primaryColor = Color(0xFF171717);
+}
