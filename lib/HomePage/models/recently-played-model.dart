7fc2d809969412e6c4be1d6bfe404ac4430381c7
radwaadel17 <radwaadel959@gmail.com>
2026-08-24 22:37:38 +0300

    feat: add RecentlyPlayedModel class to represent recently played songs with relevant details

---
+++ lib/HomePage/models/recently-played-model.dart
@@ -0,0 +1,9 @@
+// ignore: file_names
+class RecentlyPlayedModel{
+  final String songName;
+  final String singerName;
+  final String title ; 
+  final String image;
+  final String mins ; 
+  RecentlyPlayedModel(this.title, this.image, this.mins, {required this.songName,required this.singerName});
+}
